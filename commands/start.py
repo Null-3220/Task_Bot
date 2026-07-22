667faa97b542b40549ccf12a099ecc2d6cae62ff
Amiryasin <null32209@gmail.com>
2026-07-22 23:08:55 +0330

    Complete the start file

--- commands/start.py
+++ commands/start.py
@@ -2,6 +2,9 @@ def register(bot):
     @bot.message_handler(commands=['start'])
     def start_command(message):
         bot.send_message(message.chat.id, "Welcome to the task manager bot!")
+        # Start command
+
     @bot.message_handler(commands=['help'])
     def help_command(message):
         bot.send_message(message.chat.id, "Available commands:\n/start - Start the bot\n/help - Show this help message\n/add - Add a new task\n/remove - Remove a task\n/list - List all tasks") 
+        # Help command
